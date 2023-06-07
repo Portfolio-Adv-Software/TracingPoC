eee485f5269ab3ab6e9c61147aae14b5fbf00a51
Evers419 <78910809+Evers419@users.noreply.github.com>
2023-06-07 14:16:35 +0200

    stdout export

---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.17.0"
+	"log"
+	"os"
+	"os/signal"
+)
+
+func main() {
+	l := log.New(os.Stdout, "", 0)
+
+	//f, err := os.Create("traces.txt")
+	//if err != nil {
+	//	l.Fatal(err)
+	//}
+	//defer f.Close()
+
+	ctx := context.Background()
+	exp, err := installExportPipeline(ctx)
+	if err != nil {
+		l.Fatal(err)
+	}
+
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+		trace.WithResource(newResource()),
+	)
+	defer func() {
+		if err := tp.Shutdown(context.Background()); err != nil {
+			l.Fatal(err)
+		}
+	}()
+	otel.SetTracerProvider(tp)
+
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	errCh := make(chan error)
+	app := NewApp(os.Stdin, l)
+	go func() {
+		errCh <- app.Run(context.Background())
+	}()
+
+	select {
+	case <-sigCh:
+		l.Println("\ngoodbye")
+		return
+	case err := <-errCh:
+		if err != nil {
+			l.Fatal(err)
+		}
+	}
+}
+
+//// newExporter returns a console exporter.
+//func newExporter(w io.Writer) (trace.SpanExporter, error) {
+//	return stdouttrace.New(
+//		stdouttrace.WithWriter(w),
+//		// Use human-readable output.
+//		stdouttrace.WithPrettyPrint(),
+//	)
+//}
+
+func installExportPipeline(ctx context.Context) (func(context.Context) error, error) {
+	client := otlptracehttp.NewClient()
+	exporter, err := otlptrace.New(ctx, client)
+	if err != nil {
+		return nil, fmt.Errorf("creating OTLP trace exporter: %w", err)
+	}
+}
+
+func newResource() *resource.Resource {
+	r, _ := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceName("PoC"),
+			semconv.ServiceVersion("v1.0.0"),
+			attribute.String("environment", "demo"),
+		),
+	)
+	return r
+}
